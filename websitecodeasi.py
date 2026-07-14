55dcbbc0ee76f10e2c69d5f980d1bd429a43c2e6
Matej_Cizek <cizekmatysek@gmail.com>
2026-07-14 19:42:01 +0200

    Update websitecodeasi.py

--- websitecodeasi.py
+++ websitecodeasi.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 # Set the title of the website
-st.title("evil fucking token graber")
+st.title("My Python Web App")
 
-while 1:
-    st.write((input("text prosiim: ")))
+# Create a text input field for the user
+user_input = st.text_input("Enter some text here:")
+
+# If the user has typed something, display it back to them
+if user_input:
+    st.write(f"You typed: {user_input}")
